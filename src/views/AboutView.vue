96e2b767682b4ca2d79867ccb90ee870e854d191
Leonardo Barroca <barroca.leonardo@gmail.com>
2024-03-24 11:44:00 -0300

    feat: first version

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="file" @change="handleFileUpload">
+    <button @click="uploadImage">Enviar Imagem</button>
+    <div v-if="imageUrl">
+      <img :src="imageUrl" alt="Imagem Recebida">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null,
+      imageUrl: null
+    };
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.file = event.target.files[0];
+    },
+    async uploadImage() {
+      const formData = new FormData();
+      formData.append('image', this.file);
+
+      try {
+        const response = await fetch('http://localhost:5000/upload', {
+          method: 'POST',
+          body: formData
+        });
+        const data = await response.blob(); // Retorna os dados da imagem como um blob
+        const imageUrl = URL.createObjectURL(data); // Cria uma URL para o blob
+        this.imageUrl = imageUrl; // Define a URL da imagem para exibição
+      } catch (error) {
+        console.error('Erro ao enviar imagem:', error);
+      }
+    }
+  }
+};
+</script>
