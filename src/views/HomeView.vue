f6ca6e0d1448a05cb1ed5e99b69af24dc1e6afcb
Leonardo Barroca <barroca.leonardo@gmail.com>
2024-03-26 21:11:17 -0300

    fix: add vercel json and adjust home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
-  homeviewadsasdasdasdasd
-
+  <div class="presentation-page">
+    <h1 class="title">Bem-vindo ao SIE (Simple Image Editor)</h1>
+    <p class="description">Explore, Descubra, Aventure-se!</p>
+    <p class="highlight">Selecione, edite o RGB de sua imagem, baixe gratuitamente sem marcas d'água</p>
+  </div>
 </template>
+
+<style>
+.presentation-page {
+  text-align: center;
+  padding: 50px;
+}
+
+.title {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+.description {
+  font-size: 18px;
+  color: #666;
+  margin-bottom: 30px;
+}
+
+.highlight {
+  font-size: 24px;
+  color: #ff6600;
+  font-style: italic;
+}
+</style>
